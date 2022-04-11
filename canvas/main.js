18358e8f9b7110009a6e4bd518b78ba4a087b84c
Fishezzz <33294032+Fishezzz@users.noreply.github.com>
2022-04-11 02:44:04 +0200

    almost works, why is this so hard

--- canvas/main.js
+++ canvas/main.js
@@ -1,13 +1,19 @@
 // start: 1648764000000
 //   end: 1649064000000
-const SIZE = 2000;
-
 function pixelsLoaded() {
     slider.value = 1649064000000;
-    sliderChanged();
+    updateTime();
 
+    // add eventListeners
     dragElement(canvas);
-    slider.onchange = sliderChanged;
+    slider.onchange = updateTime;
+    zoom_in.onclick = zoomIn;
+    zoom_out.onclick = zoomOut;
+    window.onresize = () => {
+        pixel_ratio_X = window.screen.width / window.screen.availWidth;
+        pixel_ratio_Y = window.screen.height / window.screen.availHeight;
+        updateView();
+    }
 }
 
 function findPixel(x, y, timestamp) {
@@ -21,42 +27,75 @@ function toDataString(timestamp) {
     return date.toString().split(' GMT')[0] + "." + date.getTime().toString().substring(0, 3);
 }
 
-/*
-    function updateCanvas(timestamp, x1 = 0, y1 = 0, x2 = 1999, y2 = 1999) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        for (let x = x1; x <= x2; x++) {
-            for (let y = y1; y <= y2; y++) {
-                ctx.fillStyle = findPixel(x, y, timestamp).pixel_color;
-                ctx.fillRect(x, y, scale, scale);
-            }
-        }
-    }
-*/
 function updateCanvas(timestamp) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    let [x1, y1] = [...corner1];
-    let [x2, y2] = [...corner2];
+    let x1 = Math.max(Math.min(SIZE - 1, view.corner1.x), 0);
+    let y1 = Math.max(Math.min(SIZE - 1, view.corner1.y), 0);
+    let x2 = Math.min(Math.max(0, view.corner2.x), SIZE - 1);
+    let y2 = Math.min(Math.max(0, view.corner2.y), SIZE - 1);
+
+    let log = '';
+    log += x1 === view.corner1.x ? '   ' : 'x1 ';
+    log += y1 === view.corner1.y ? '   ' : 'y1 ';
+    log += x2 === view.corner2.x ? '   ' : 'x2 ';
+    log += y2 === view.corner2.y ? '   ' : 'y2 ';
+    console.log(log);
 
     for (let x = x1; x <= x2; x++) {
         for (let y = y1; y <= y2; y++) {
             ctx.fillStyle = findPixel(x, y, timestamp).pixel_color;
-            ctx.fillRect(x, y, scale, scale);
+            ctx.fillRect(x - x1, y - y1, 1, 1);
         }
     }
 }
 
-
-function sliderChanged() {
+function updateTime() {
     datetime.innerHTML = toDataString(slider.value);
-    // updateCanvas(slider.value, ...corner1, ...corner2);
+    updateCanvas(slider.value);
+}
+
+function updateView(changeX = 0, changeY = 0) {
+    // check for borders
+    if (view.center.x + changeX < 0) {
+        changeX = 0 - view.center.x;
+    } else if (view.center.x + changeX > SIZE - 1) {
+        changeX = SIZE - 1 - view.center.x;
+    }
+    if (view.center.y + changeY < 0) {
+        changeY = 0 - view.center.y;
+    } else if (view.center.y + changeY > SIZE - 1) {
+        changeY = SIZE - 1 - view.center.y;
+    }
+
+    // update center
+    view.center.x += changeX;
+    view.center.y += changeY;
+
+    // sanity checks
+    if (view.center.x < -2000 || view.center.x >= 2000 || view.center.y < -2000 || view.center.y >= 2000) {
+        alert("IT FUCKED");
+        view.center.x = HCW;
+        view.center.y = HCH;
+    }
+
+
+    // update corners
+    view.corner1.x = view.center.x - HCW / scale;
+    view.corner1.y = view.center.y - HCH / scale;
+
+    view.corner2.x = view.center.x + HCW / scale;
+    view.corner2.y = view.center.y + HCH / scale;
+
+    console.log(view.corner1, view.center, view.corner2);
+
+    // update canvas
     updateCanvas(slider.value);
 }
 
 // movement
 function dragElement(elem) {
-    let newX = 0, newY = 0, oldX = 0, oldY = 0;
+    let changeX = 0, changeY = 0, oldX = 0, oldY = 0;
 
     elem.onmousedown = startDragElement;
     elem.ontouchstart = touchStart;
@@ -68,8 +107,8 @@ function dragElement(elem) {
         e.preventDefault();
 
         // get the mouse cursor position at startup
-        oldX = e.clientX / 5;
-        oldY = e.clientY / 5;
+        oldX = e.clientX;
+        oldY = e.clientY;
 
         document.onmouseup = endDragElement;
         document.onmousemove = elementDrag;
@@ -81,14 +120,15 @@ function dragElement(elem) {
         e.preventDefault();
 
         // calculate the new cursor position
-        newX = oldX - e.clientX / 5;
-        newY = oldY - e.clientY / 5;
-        oldX = e.clientX / 5;
-        oldY = e.clientY / 5;
+        changeX = oldX - e.clientX;
+        changeY = oldY - e.clientY;
+        oldX = e.clientX;
+        oldY = e.clientY;
 
         // change in positive X: 50 -> 100 => newX = 50 - 100 = -50
         // changes will always be negative / inverted values of their direction
-        updateView(newX * -1, newY * -1);
+        // updateView(newX * -0.1, newY * -0.1);
+        updateView(changeX * pixel_ratio_X, changeY * pixel_ratio_Y);
     }
 
     // end of dragging
@@ -119,14 +159,15 @@ function dragElement(elem) {
         e.preventDefault();
 
         // calculate the new position
-        newX = oldX - e.touches[0].clientX;
-        newY = oldY - e.touches[0].clientY;
+        changeX = oldX - e.touches[0].clientX;
+        changeY = oldY - e.touches[0].clientY;
         oldX = e.touches[0].clientX;
         oldY = e.touches[0].clientY;
 
         // change in positive X: 50 -> 100 => newX = 50 - 100 = -50
         // changes will always be negative / inverted values of their direction
-        updateView(newX * -1, newY * -1);
+        // updateView(newX * -0.1, newY * -0.1);
+        updateView(changeX * pixel_ratio_X, changeY * pixel_ratio_Y);
     }
 
     // end of touch
@@ -136,68 +177,24 @@ function dragElement(elem) {
         document.ontouchend = null;
         document.ontouchmove = null;
     }
-
-    function updateView(changeX = 0, changeY = 0) {
-        // For pixels
-        if (changeX > 0) {
-            if (corner2[0] + changeX > SIZE - 1) {
-                changeX = SIZE - 1 - corner2[0];
-            }
-        } else {
-            if (corner1[0] + changeX < 0) {
-                changeX = 0 - corner1[0];
-            }
-        }
-        corner1[0] = Math.floor(corner1[0] + changeX);
-        corner2[0] = Math.floor(corner2[0] + changeX);
-
-        if (changeY > 0) {
-            if (corner2[1] + changeY > SIZE - 1) {
-                changeY = SIZE - 1 - corner2[1];
-            }
-        } else {
-            if (corner1[1] + changeY < 0) {
-                changeY = 0 - corner1[1];
-            }
-        }
-        corner1[1] = Math.floor(corner1[1] + changeY);
-        corner2[1] = Math.floor(corner2[1] + changeY);
-
-        //// For canvas
-        // if (changeX > 0) {
-        //     if (corner1[0] + changeX > HCW) {
-        //         changeX = HCW - corner1[0];
-        //     }
-        // } else {
-        //     if (corner2[0] + changeX < HCW) {
-        //         changeX = HCW - corner2[0];
-        //     }
-        // }
-        // corner1[0] = Math.floor(corner1[0] + changeX);
-        // corner2[0] = Math.floor(corner2[0] + changeX);
-
-        // if (changeY > 0) {
-        //     if (corner1[1] + changeY > HCH) {
-        //         changeY = HCH - corner1[0];
-        //     }
-        // } else {
-        //     if (corner2[1] + changeY < HCH) {
-        //         changeY = HCH - corner2[1];
-        //     }
-        // }
-        // corner1[1] = Math.floor(corner1[1] + changeY);
-        // corner2[1] = Math.floor(corner2[1] + changeY);
-
-        sliderChanged();
-    }
 }
 
 function zoomIn() {
-
+    if (scale < SCALE_MAX) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        scale *= 2
+        ctx.scale(2, 2);
+        updateCanvas(slider.value);
+    }
 }
 
 function zoomOut() {
-
+    if (scale > SCALE_MIN) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        scale /= 2
+        ctx.scale(0.5, 0.5);
+        updateCanvas(slider.value);
+    }
 }
 
 let datetime = document.getElementById("datetime");
@@ -207,20 +204,28 @@ let zoom_in = document.getElementById("zoom_in");
 
 let canvas = document.getElementsByTagName("canvas")[0];
 let ctx = canvas.getContext('2d');
+canvas.width = 500;
+canvas.height = 500;
+
+const SIZE = 2000;
+const SCALE_MIN = 0.5;
+const SCALE_MAX = 4;
+
+let scale = 2;
+
+let pixel_ratio_X = window.screen.width / window.screen.availWidth;
+let pixel_ratio_Y = window.screen.height / window.screen.availHeight;
 
 /** half the canvas width */
 const HCW = canvas.width / 2;
 /** half the canvas height */
 const HCH = canvas.height / 2;
 
-// to scale pixel coordinates to canvas coordinates
-let scale = 1;
-// all true pixel coordinates
-let corner1 = [0, 0];
-let corner2 = [canvas.width, canvas.height];
-let center = () => [
-    Math.floor((corner1[0] + corner2[0]) / 2),
-    Math.floor((corner1[1] + corner2[1]) / 2)
-];
+// reference point in pixel coordinates
+let view = {
+    center: { x: HCW, y: HCH },
+    corner1: { x: 0, y: 0 },
+    corner2: { x: canvas.width, y: canvas.height }
+}
 
 loadPixelData().then(() => pixelsLoaded);
